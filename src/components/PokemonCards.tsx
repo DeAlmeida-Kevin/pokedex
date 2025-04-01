f4ebaee60232391a8c96bac584a43b8a9499071e
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-04-01 15:44:18 +0200

    add pokemonList

--- src/components/PokemonCards.tsx
+++ src/components/PokemonCards.tsx
@@ -1,10 +1,26 @@
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
+
 
 function PokemonCard() {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="C'est un bulbizarre" />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+    const pokemon = pokemonList[1]
+
+    return (
+        <figure>
+            {(pokemon.imgSrc) ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+    )
 }
 
 export default PokemonCard;
