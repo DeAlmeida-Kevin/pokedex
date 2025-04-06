591e0e45a83315f23f6c1a71c2e1f996ade2cc4a
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-04-06 15:56:07 +0200

    fin exo react bacis7

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,24 @@ const pokemonList = [
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
-		imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
 	},
 ];
 
@@ -27,15 +43,18 @@ function App() {
 
 
 	return (
+
 		<div>
+			<nav>
+				{pokemonList.map((pokemon) => (<button type="button" key={pokemon.name}
+					onClick={() => setPokemonName(pokemon.name)}>{pokemon.name}</button>))}
+			</nav>
+
 			<PokemonCard pokemon={pokemon} />
-			<button type="button" onClick={() => setPokemonName("bulbasaur")}>
-				Bulbasaur
-			</button>
-			<button type="button" onClick={() => setPokemonName("mew")}>
-				Mew
-			</button>
 		</div >
+
+
+
 	);
 }
 
